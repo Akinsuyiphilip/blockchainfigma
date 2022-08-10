61117ee95f966733fb5f412efd51f33683a83d6c
Akinsuyiphilip <philipakinbolade@gmail.com>
2022-08-10 07:35:26 +0100

    files

---
+++ src/components/Howto.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function Howto() {
+return (
+<>
+    <div className="howto">
+        <h2>
+            How Do We Work Our System and Our Journey
+        </h2>
+        <div className="consulting">
+            <div className="textlogo">
+                <img src="" alt="" />
+                <div className="texts">
+                    <h3>Consulting</h3>
+                    <p>A Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+                        the industry's standard dummy text ever since the 1500s, </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</>
+)
+}
+
+export default Howto
